9c8d3145b86a96f7b00fc1530b24c65de9e8c099
Mohammad Mujahidul Islam <islamm13@myumanitoba.ca>
2025-05-15 07:24:08 +0000

    deleted some comments

--- physician-connect/src/main/java/physicianconnect/presentation/AppointmentListView.java
+++ physician-connect/src/main/java/physicianconnect/presentation/AppointmentListView.java
@@ -1,11 +1,9 @@
 package physicianconnect.presentation;
 
-
 import java.util.List;
 
 import physicianconnect.objects.Appointment;
 
-//managge upcoming appointments
 public interface AppointmentListView extends Viewable {
     void setAppointments(List<Appointment> appointments);
     // void showUpcomingAppointments(List<Appointment> appointments);
